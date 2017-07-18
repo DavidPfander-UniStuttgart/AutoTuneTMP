9bedfb4b925e2858dfd27a6919bc75dd12d03c6b
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-07-18 09:47:51 +0200

    some refactoring

--- examples/loop_nest_example.cpp
+++ examples/loop_nest_example.cpp
@@ -10,8 +10,9 @@ int main(void) {
   std::vector<double> arr(N * N);
   std::fill(arr.begin(), arr.end(), 0.0);
 
-  opttmp::loop::loop_nest<2, double>(
-      {0, 0}, {N, N}, {1, 1}, [&arr](auto (&i)[2]) { arr[i[0] * N + i[1]] = 3.0; });
+  opttmp::loop::dynamic_loop_nest<2, size_t>(
+      {0, 0}, {N, N}, {1, 1},
+      [&arr](std::array<size_t, 2> &i) { arr[i[0] * N + i[1]] = 3.0; });
 
   for (size_t i = 0; i < N * N; i++) {
     std::cout << "i: " << i << " -> " << arr[i] << std::endl;
--- include/opttmp/loop/loop_nest.hpp
+++ include/opttmp/loop/loop_nest.hpp
@@ -9,59 +9,38 @@ namespace loop {
 
 namespace detail {
 
-// template recursion for c-style arrays
-template <size_t dim, size_t cur_dim, typename T, typename F>
-typename std::enable_if<cur_dim == dim, void>::type
-execute_looped(const T (&)[dim], const T (&)[dim], const T (&)[dim],
-               T (&completed_index)[dim], F f) {
-  f(completed_index);
-}
-
-template <size_t dim, size_t cur_dim, typename T, typename F>
-typename std::enable_if<cur_dim != dim, void>::type
-execute_looped(const T (&min)[dim], const T (&max)[dim], const T (&step)[dim],
-               T (&partial_index)[dim], F f) {
-  for (T cur = min[cur_dim]; cur < max[cur_dim]; cur += step[cur_dim]) {
-    partial_index[cur_dim] = cur;
-    execute_looped<dim, cur_dim + 1>(min, max, step, partial_index, f);
-  }
-}
-
 // template recursion for std::array
-template <size_t dim, size_t cur_dim, typename T, typename F>
+template <size_t dim, size_t cur_dim, typename index_type, typename F>
 typename std::enable_if<cur_dim == dim, void>::type
-execute_looped(const std::array<T, dim> &, const std::array<T, dim> &,
-               const std::array<T, dim> &, std::array<T, dim> &completed_index,
-               F f) {
+dynamic_loop_nest(const std::array<index_type, dim> &,
+                  const std::array<index_type, dim> &,
+                  const std::array<index_type, dim> &,
+                  std::array<index_type, dim> &completed_index, F f) {
   f(completed_index);
 }
 
-template <size_t dim, size_t cur_dim, typename T, typename F>
+template <size_t dim, size_t cur_dim, typename index_type, typename F>
 typename std::enable_if<cur_dim != dim, void>::type
-execute_looped(const std::array<T, dim> &min, const std::array<T, dim> &max,
-               const std::array<T, dim> &step,
-               std::array<T, dim> &partial_index, F f) {
-  for (T cur = min[cur_dim]; cur < max[cur_dim]; cur += step[cur_dim]) {
+dynamic_loop_nest(const std::array<index_type, dim> &min,
+                  const std::array<index_type, dim> &max,
+                  const std::array<index_type, dim> &step,
+                  std::array<index_type, dim> &partial_index, F f) {
+  for (index_type cur = min[cur_dim]; cur < max[cur_dim];
+       cur += step[cur_dim]) {
     partial_index[cur_dim] = cur;
-    execute_looped<dim, cur_dim + 1>(min, max, step, partial_index, f);
+    dynamic_loop_nest<dim, cur_dim + 1>(min, max, step, partial_index, f);
   }
 }
-}
-
-// overload for c-style array
-template <size_t dim, typename T, typename F>
-void loop_nest(const T (&min)[dim], const T (&max)[dim], const T (&step)[dim],
-               F f) {
-  T partial_index[dim];
-  detail::execute_looped<dim, 0>(min, max, step, partial_index, f);
 }
 
 // overload for std::array
-template <size_t dim, typename T, typename F>
-void loop_nest(const std::array<T, dim> &min, const std::array<T, dim> &max,
-               const std::array<T, dim> &step, F f) {
-  std::array<T, dim> partial_index;
-  detail::execute_looped<dim, 0>(min, max, step, partial_index, f);
+template <size_t dim, typename index_type, typename F>
+void dynamic_loop_nest(const std::array<index_type, dim> &min,
+                       const std::array<index_type, dim> &max,
+                       const std::array<index_type, dim> &step, F f) {
+  std::array<index_type, dim> partial_index;
+  detail::dynamic_loop_nest<dim, 0>(min, max, step, partial_index, f);
 }
-}
-}
+
+} // namespace loop
+} // namespace opttmp
