c169c6e2c782ca1d7cd32bf8b451b0b59cc08a56
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-08-23 14:23:03 +0200

    added missing files

---
+++ examples/kernels_unroll_loop_autotune_return_int/unrolling_kernel.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+#include "opttmp/loop/unroll_loop.hpp"
+
+#include "parameters.hpp"
+
+extern "C" int unrolling_kernel(std::vector<double> &arr, const size_t N) {
+  opttmp::loop::unroll_loop<6, 14, 2>([&arr](auto i) { arr[i] = 3.0; });
+  std::cout << "UNROLL_LOOP is: " << UNROLL_LOOP << std::endl;
+#if UNROLL_LOOP == 1
+  std::cout << "UNROLL_LOOP is defined!" << std::endl;
+#endif
+  return 5;
+}
---
+++ examples/unroll_loop_autotune_return_int.cpp
@@ -0,0 +1,45 @@
+#include "autotune/autotune.hpp"
+#include "autotune/parameter.hpp"
+#include "autotune/tuners/line_search.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+// defines kernel, put in single compilation unit
+AUTOTUNE_DECLARE_DEFINE_KERNEL(int(std::vector<double> &, const size_t),
+                               unrolling_kernel)
+
+int main(void) {
+  size_t N = 20;
+
+  std::vector<double> arr(N);
+  std::fill(arr.begin(), arr.end(), 0.0);
+
+  autotune::unrolling_kernel.set_verbose(true);
+
+  auto builder =
+      autotune::unrolling_kernel.get_builder_as<cppjit::builder::gcc>();
+  // assuming the example is run from the repository base folder
+  builder->set_include_paths("-I src");
+  builder->set_cpp_flags("-std=c++1z -fPIC");
+  builder->set_link_flags("-std=c++1z -shared");
+
+  autotune::unrolling_kernel.set_source_dir(
+      "examples/kernels_unroll_loop_autotune_return_int");
+
+  autotune::unrolling_kernel.add_parameter("UNROLL_LOOP", {"0", "1"});
+  autotune::unrolling_kernel.add_parameter("DUMMY_PAR_2", {"0", "1", "2", "3"});
+  autotune::unrolling_kernel.add_parameter("DUMMY_PAR_3", {"a", "b", "c", "d"});
+
+  std::vector<size_t> line_search_initial_guess = {0, 0, 0};
+  autotune::tuners::line_search<decltype(autotune::unrolling_kernel)> tuner(
+      autotune::unrolling_kernel, 50, 1, line_search_initial_guess);
+
+  tuner.setup_tests([](int r) { return r == 5 ? true : false; });
+
+  std::vector<size_t> optimal_indices = tuner.tune(arr, N);
+
+  autotune::unrolling_kernel.print_values(optimal_indices);
+  return 0;
+}
