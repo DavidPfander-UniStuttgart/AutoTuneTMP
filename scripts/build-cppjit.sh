24ca61c44ad4e04dccaabd816a8b8a151d705fd0
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-11-30 15:24:36 +0100

    another build system test

--- scripts/build-cppjit.sh
+++ scripts/build-cppjit.sh
@@ -2,12 +2,6 @@
 set -x
 set -e
 
-if [ -z ${AutoTuneTMP_source_me_sourced} ] ; then
-    source source-me.sh
-else
-    echo "AutoTuneTMP: source-me.sh already sourced"
-fi
-
 if [ ! -d "cppjit" ]; then
     git clone git@github.com:DavidPfander-UniStuttgart/cppjit.git
 fi
