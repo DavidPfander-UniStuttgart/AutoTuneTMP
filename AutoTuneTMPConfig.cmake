81e0485e7287b03987efc262e322031b63c06eba
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-12-01 13:16:51 +0100

    testing better include directories

--- AutoTuneTMPConfig.cmake
+++ AutoTuneTMPConfig.cmake
@@ -1,3 +1,4 @@
 include(CMakeFindDependencyMacro)
+find_package(Vc ${Vc_FIND_VERSION} QUIET NO_MODULE PATHS ${Vc_ROOT})
 find_package(cppjit PATHS ${CPPJIT_ROOT})
 include("${CMAKE_CURRENT_LIST_DIR}/AutoTuneTMPTargets.cmake")
