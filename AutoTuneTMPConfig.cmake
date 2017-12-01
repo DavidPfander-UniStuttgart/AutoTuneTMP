d7f6c68bea5930098c44cf6126f0dbdcd6969af7
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-12-01 14:57:22 +0100

    another build system fix

--- AutoTuneTMPConfig.cmake
+++ AutoTuneTMPConfig.cmake
@@ -1,4 +1,7 @@
 include(CMakeFindDependencyMacro)
-find_package(Vc ${Vc_FIND_VERSION} QUIET NO_MODULE PATHS ${Vc_ROOT})
+# find_package(Vc ${Vc_FIND_VERSION} NO_MODULE PATHS ${Vc_ROOT})
 find_package(cppjit PATHS ${CPPJIT_ROOT})
 include("${CMAKE_CURRENT_LIST_DIR}/AutoTuneTMPTargets.cmake")
+# target_include_directories(AutoTuneTMP INTERFACE
+#   $<BUILD_INTERFACE:${Vc_INCLUDE_DIR}>
+#   )
