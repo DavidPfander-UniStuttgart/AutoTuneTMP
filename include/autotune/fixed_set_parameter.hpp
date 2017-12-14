63fce640928892fc5b39be34e6743153282382e2
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-12-14 18:24:54 +0100

    still moving towards AutoTuneTMP

--- include/autotune/fixed_set_parameter.hpp
+++ include/autotune/fixed_set_parameter.hpp
@@ -8,23 +8,36 @@
 
 namespace autotune {
 
-class fixed_set_parameter : public abstract_parameter {
+template <typename T> class fixed_set_parameter;
+
+template <>
+class fixed_set_parameter<const char *> : public abstract_parameter {
 private:
   size_t cur_index;
   std::vector<std::string> values;
 
 public:
+  // fixed_set_parameter(const std::string &name,
+  //                     const std::vector<const char *> &values)
+  //     : abstract_parameter(name), cur_index(0) {
+  //   for (size_t i = 0; i < values.size(); i++) {
+  //     this->values.insert(this->values.end(), values.begin(), values.end());
+  //     // this->values.push_back(values[i]);
+  //   }
+  // }
+
   fixed_set_parameter(const std::string &name,
-                      const std::vector<std::string> &values)
-      : abstract_parameter(name), cur_index(0), values(values) {}
+                      std::initializer_list<const char *> values)
+      : abstract_parameter(name), cur_index(0) {
+    for (size_t i = 0; i < values.size(); i++) {
+      this->values.insert(this->values.end(), values.begin(), values.end());
+      // this->values.push_back(values[i]);
+    }
+  }
 
   const std::vector<std::string> &get_values() const { return this->values; }
 
   void set_index(size_t new_index) { cur_index = new_index; };
-
-  // const std::string &get_value(size_t index) const {
-  //   return this->values[index];
-  // }
 
   virtual const std::string get_value() const override {
     return std::string("\"") + this->values[cur_index] + std::string("\"");
@@ -56,21 +69,50 @@ public:
     // TODO: should be extended, so that an initial guess can be supplied
     cur_index = 0;
   }
+};
 
-  // virtual std::string to_parameter_source_line() override {
-  //   return "#define " + name + " " + values[cur_index] + "\n";
-  // }
+template <typename T> class fixed_set_parameter : public abstract_parameter {
+private:
+  size_t cur_index;
+  std::vector<std::string> values;
 
-  // TODO: remove this overload?
-  // std::string to_parameter_source_line(size_t index) {
-  //   return "#define " + name + " " + values[index] + "\n";
-  // }
+public:
+  fixed_set_parameter(const std::string &name, const std::vector<T> &values)
+      : abstract_parameter(name), cur_index(0), values(values) {}
 
-  // virtual std::shared_ptr<fixed_set_parameter> clone() override {
-  //   std::shared_ptr<fixed_set_parameter> new_instance =
-  //       std::make_shared<fixed_set_parameter>(this->name, this->values);
-  //   new_instance->cur_index = this->cur_index;
-  //   return new_instance;
-  // };
+  const std::vector<std::string> &get_values() const { return this->values; }
+
+  void set_index(size_t new_index) { cur_index = new_index; };
+
+  virtual const std::string get_value() const override {
+    return this->values[cur_index];
+  }
+
+  size_t count_values() const { return values.size(); }
+
+  bool next() {
+    if (cur_index + 1 < values.size()) {
+      cur_index += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool prev() {
+    if (cur_index > 0) {
+      cur_index -= 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void set_min() { cur_index = 0; };
+
+  virtual void set_initial() {
+    // TODO: should be extended, so that an initial guess can be supplied
+    cur_index = 0;
+  }
 };
 }
