1b193fe8c5c2c0a5d0bc23bd58da5d1275fd9fa1
pfandedd <David.Pfander@ipvs.uni-stuttgart.de>
2017-12-29 21:00:19 +0100

    added missing files

---
+++ include/opttmp/vectorization/expression.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstddef>
+
+namespace opttmp {
+namespace vectorization {
+
+// base type, everything is a expression
+// back-reference to specialized type though argument
+// curiously-recurring template pattern
+template <typename specialized_expression_type, typename vc_type,
+          size_t elements>
+class expression {
+public:
+  using expr_vc_type = vc_type;
+  static constexpr size_t expr_elements = elements;
+
+  const vc_type operator[](size_t i) const {
+    // specialized_expression_type &spec =
+    //     static_cast<specialized_expression_type &>(*this);
+    // return spec.operator[](i);
+    return static_cast<specialized_expression_type const &>(*this)[i];
+  }
+
+  size_t size() const { return elements; }
+
+  // The following overload conversions to E, the template argument type;
+  // e.g., for register_tiling_expression<sum_expression>, this is a conversion
+  // to sum_expression.
+  // operator E &() { return static_cast<E &>(*this); }
+  // operator const E &() const { return static_cast<const E &>(*this); }
+};
+}
+}
---
+++ include/opttmp/vectorization/register_tiling.hpp
@@ -0,0 +1,313 @@
+#pragma once
+
+#include "expression.hpp"
+#include "register_array.hpp"
+
+#include <Vc/Vc>
+
+namespace opttmp {
+namespace vectorization {
+
+/////////////////////////////////////////////////
+// expressions representing different operations
+/////////////////////////////////////////////////
+
+// expression for sums
+template <typename left_expr, typename right_expr>
+class sum_expression : public expression<sum_expression<left_expr, right_expr>,
+                                         typename left_expr::expr_vc_type,
+                                         left_expr::expr_elements> {
+
+  const left_expr &u;
+  const right_expr &v;
+
+public:
+  sum_expression(const left_expr &u, const right_expr &v) : u(u), v(v) {}
+
+  typename left_expr::expr_vc_type operator[](size_t i) const {
+    return u[i] + v[i];
+  }
+
+  size_t size() const { return left_expr::expr_elements; }
+};
+
+template <typename left_expr, typename scalar>
+class sum_scalar_expression
+    : public expression<sum_scalar_expression<left_expr, scalar>,
+                        typename left_expr::expr_vc_type,
+                        left_expr::expr_elements> {
+
+  left_expr const &u;
+  scalar const &s;
+
+public:
+  sum_scalar_expression(left_expr const &u, scalar const &s) : u(u), s(s) {}
+
+  typename left_expr::expr_vc_type operator[](size_t i) const {
+    return u[i] + s;
+  }
+
+  size_t size() const { return left_expr::expr_elements; }
+};
+
+// expression for sub
+template <typename left_expr, typename right_expr>
+class sub_expression : public expression<sub_expression<left_expr, right_expr>,
+                                         typename left_expr::expr_vc_type,
+                                         left_expr::expr_elements> {
+
+  left_expr const &u;
+  right_expr const &v;
+
+public:
+  sub_expression(left_expr const &u, right_expr const &v) : u(u), v(v) {}
+
+  typename left_expr::expr_vc_type operator[](size_t i) const {
+    return u[i] - v[i];
+  }
+
+  size_t size() const { return left_expr::expr_elements; }
+};
+
+template <typename left_expr, typename scalar>
+class sub_scalar_expression
+    : public expression<sub_scalar_expression<left_expr, scalar>,
+                        typename left_expr::expr_vc_type,
+                        left_expr::expr_elements> {
+
+  left_expr const &u;
+  scalar const &s;
+
+public:
+  sub_scalar_expression(left_expr const &u, scalar const &s) : u(u), s(s) {}
+
+  typename left_expr::expr_vc_type operator[](size_t i) const {
+    return u[i] + s;
+  }
+
+  size_t size() const { return left_expr::expr_elements; }
+};
+
+// expression for multiplication
+template <typename left_expr, typename right_expr>
+class mult_expression
+    : public expression<mult_expression<left_expr, right_expr>,
+                        typename left_expr::expr_vc_type,
+                        left_expr::expr_elements> {
+
+  left_expr const &u;
+  right_expr const &v;
+
+public:
+  mult_expression(left_expr const &u, right_expr const &v) : u(u), v(v) {}
+
+  typename left_expr::expr_vc_type operator[](size_t i) const {
+    return u[i] * v[i];
+  }
+
+  size_t size() const { return left_expr::expr_elements; }
+};
+
+template <typename left_expr, typename scalar>
+class mult_scalar_expression
+    : public expression<mult_expression<left_expr, scalar>,
+                        typename left_expr::expr_vc_type,
+                        left_expr::expr_elements> {
+
+  left_expr const &u;
+  scalar const &s;
+
+public:
+  mult_scalar_expression(left_expr const &u, scalar const &s) : u(u), s(s) {}
+
+  typename left_expr::expr_vc_type operator[](size_t i) const {
+    return u[i] * s;
+  }
+
+  size_t size() const { return left_expr::expr_elements; }
+};
+
+// expression for unary sub
+template <typename left_expr>
+class sub_unary_expression : public expression<sub_unary_expression<left_expr>,
+                                               typename left_expr::expr_vc_type,
+                                               left_expr::expr_elements> {
+
+  left_expr const &u;
+
+public:
+  sub_unary_expression(left_expr const &u) : u(u) {}
+
+  typename left_expr::expr_vc_type operator[](size_t i) const { return -u[i]; }
+
+  size_t size() const { return left_expr::expr_elements; }
+};
+
+/////////////////////////////////
+// operators for syntactic sugar
+/////////////////////////////////
+
+// cannot work, because different types of expressions are valid and data
+// classes as well
+// typename std::enable_if<std::is_same<left_expr, right_expr>::value,
+//                         sum_expression<left_expr, right_expr>>::type
+
+// // overloaded operator for addition
+// template <typename left_expr, typename right_expr>
+// const left_expr operator+(left_expr const &u, right_expr const &v) {
+//   return sum_expression<left_expr, right_expr>(u, v);
+// }
+
+// operators for sum
+template <typename left_specialized, typename right_specialized,
+          typename vc_type, size_t elements>
+const auto
+operator+(const expression<left_specialized, vc_type, elements> &u,
+          const expression<right_specialized, vc_type, elements> &v) {
+  return sum_expression<expression<left_specialized, vc_type, elements>,
+                        expression<right_specialized, vc_type, elements>>(u, v);
+}
+
+template <typename left_specialized, typename vc_type, size_t elements>
+const auto operator+(expression<left_specialized, vc_type, elements> &u,
+                     vc_type &s) {
+  return sum_scalar_expression<expression<left_specialized, vc_type, elements>,
+                               vc_type>(u, s);
+}
+
+template <typename right_specialized, typename vc_type, size_t elements>
+const auto operator+(vc_type &s,
+                     expression<right_specialized, vc_type, elements> &u) {
+  // using commutativity to avoid another class
+  return sum_scalar_expression<expression<right_specialized, vc_type, elements>,
+                               vc_type>(u, s);
+}
+
+// operators for sub
+template <typename left_specialized, typename right_specialized,
+          typename vc_type, size_t elements>
+const auto operator-(expression<left_specialized, vc_type, elements> &u,
+                     expression<right_specialized, vc_type, elements> &v) {
+  return sub_expression<expression<left_specialized, vc_type, elements>,
+                        expression<right_specialized, vc_type, elements>>(u, v);
+}
+
+template <typename left_specialized, typename vc_type, size_t elements>
+const auto operator-(expression<left_specialized, vc_type, elements> &u,
+                     vc_type &s) {
+  return sub_scalar_expression<expression<left_specialized, vc_type, elements>,
+                               vc_type>(u, s);
+}
+
+template <typename right_specialized, typename vc_type, size_t elements>
+const auto operator-(vc_type &s,
+                     expression<right_specialized, vc_type, elements> &u) {
+  // using commutativity to avoid another class
+  return sub_scalar_expression<expression<right_specialized, vc_type, elements>,
+                               vc_type>(u, s);
+}
+
+// overloaded operator for multiplication
+template <typename left_specialized, typename right_specialized,
+          typename vc_type, size_t elements>
+const auto operator*(expression<left_specialized, vc_type, elements> &u,
+                     expression<right_specialized, vc_type, elements> &v) {
+  return mult_expression<expression<left_specialized, vc_type, elements>,
+                         expression<right_specialized, vc_type, elements>>(u,
+                                                                           v);
+}
+
+template <typename left_specialized, typename vc_type, size_t elements>
+const auto operator*(expression<left_specialized, vc_type, elements> &u,
+                     vc_type &s) {
+  return mult_scalar_expression<expression<left_specialized, vc_type, elements>,
+                                vc_type>(u, s);
+}
+
+template <typename right_specialized, typename vc_type, size_t elements>
+const auto operator*(vc_type &s,
+                     expression<right_specialized, vc_type, elements> &u) {
+  // using commutativity to avoid another class
+  return mult_scalar_expression<
+      expression<right_specialized, vc_type, elements>, vc_type>(u, s);
+}
+
+template <typename specialized, typename vc_type, size_t elements>
+const auto abs(expression<specialized, vc_type, elements> &u) {
+  register_array<vc_type, elements> result; // TODO: turn into expression
+  for (size_t i = 0; i < elements; i++) {
+    result[i] = Vc::abs(u[i]);
+  }
+  return result;
+}
+
+template <typename left_specialized, typename right_specialized,
+          typename vc_type, size_t elements>
+const auto max(expression<left_specialized, vc_type, elements> &u,
+               expression<right_specialized, vc_type, elements> &v) {
+  register_array<vc_type, elements> result; // TODO: turn into expression
+  for (size_t i = 0; i < elements; i++) {
+    result[i] = Vc::max(u[i], v[i]);
+  }
+  return result;
+}
+
+template <typename left_specialized, typename vc_type, size_t elements>
+const auto max(expression<left_specialized, vc_type, elements> &u, vc_type &s) {
+  register_array<vc_type, elements> result; // TODO: turn into expression
+  for (size_t i = 0; i < elements; i++) {
+    result[i] = Vc::max(u[i], s);
+  }
+  return result;
+}
+
+template <typename right_specialized, typename vc_type, size_t elements>
+const auto max(vc_type &s,
+               expression<right_specialized, vc_type, elements> &u) {
+  register_array<vc_type, elements> result; // TODO: turn into expression
+  for (size_t i = 0; i < elements; i++) {
+    result[i] = Vc::max(u[i], s);
+  }
+  return result;
+}
+
+// operator for unary minus
+// expression for unary minus
+template <typename left_expr, typename vc_type, size_t elements>
+const auto operator-(expression<left_expr, vc_type, elements> &l) {
+  return sub_unary_expression<expression<left_expr, vc_type, elements>>(l);
+}
+
+template <typename left_specialized, typename right_specialized,
+          typename vc_type, size_t elements>
+const auto operator*=(expression<left_specialized, vc_type, elements> &u,
+                      expression<right_specialized, vc_type, elements> &v) {
+  auto mult_expr = u * v;
+  register_array<vc_type, elements> result;
+  for (size_t i = 0; i < elements; i++) {
+    result[i] = mult_expr[i];
+  }
+  return result;
+}
+
+template <typename right_specialized, // typename left_specialized,
+          typename vc_type, size_t elements>
+const auto operator+=(register_array<vc_type, elements> &u,
+                      expression<right_specialized, vc_type, elements> &v) {
+  // std::cout << "inside operator" << std::endl;
+  // for (size_t i = 0; i < u.size(); i++) {
+  //   if (i > 0) {
+  //     std::cout << ", ";
+  //   }
+  //   std::cout << u[i];
+  // }
+  // std::cout << std::endl;
+
+  auto sum_expr = u + v;
+  for (size_t i = 0; i < elements; i++) {
+    u[i] = sum_expr[i];
+  }
+  return u;
+}
+}
+}
