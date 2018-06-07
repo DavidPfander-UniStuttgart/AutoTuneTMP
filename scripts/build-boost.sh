7d3d869faa95de108538c57b3a87d2aed8672451
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2018-06-07 10:21:43 +0200

    fix to build process

--- scripts/build-boost.sh
+++ scripts/build-boost.sh
@@ -2,6 +2,10 @@
 set -x
 set -e
 
+if [[ -z ${AutoTuneTMP_source_me_sourced} ]]; then
+    source source-me.sh
+fi
+
 if [ ! -d "boost" ]; then
     wget 'http://downloads.sourceforge.net/project/boost/boost/1.65.0/boost_1_65_0.tar.bz2'
     tar xf boost_1_65_0.tar.bz2
@@ -15,10 +19,9 @@ fi
 if [ ! -d "boost_install/" ]; then
     echo "building boost"
     cd boost
-    ./bootstrap.sh --prefix="$BOOST_ROOT" -with-libraries=program_options,filesystem,system,test
+    ./bootstrap.sh --prefix="$BOOST_ROOT" -with-libraries=program_options,filesystem,system,test,thread,atomic,date_time
     ./b2 -j${PARALLEL_BUILD} variant=release install
     cd ..
 fi
-
 
 
