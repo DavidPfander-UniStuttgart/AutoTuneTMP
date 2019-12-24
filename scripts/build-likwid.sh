f5fb836778b04c2ab0fbcc4d36c466e577e96e65
David Pfander <David.Pfander@gmail.com>
2019-12-24 17:31:37 +0100

    using a specific version of likwid

--- scripts/build-likwid.sh
+++ scripts/build-likwid.sh
@@ -8,14 +8,17 @@ fi
 
 if [ ! -d "likwid/" ]; then
     git clone git@github.com:RRZE-HPC/likwid.git
+    cd likwid/
+    git checkout 4.3.4
+    cd ..
     # mkdir likwid_install
     # cd likwid
     # likwid needs absolut paths, a bit more magic needed
     # sed -i -e 's/PREFIX = \/usr\/local#NO SPACE/PREFIX = likwid_install#NO SPACE/g' config.mk
     # sed -i -e 's/INSTALL_CHOWN = -g root -o root#NO SPACE/INSTALL_CHOWN = #NO SPACE/g' config.mk
     # cd ..
-else
-    git pull
+# else
+#     git pull
 fi
 
 cd likwid
